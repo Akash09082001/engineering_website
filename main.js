10b65d9c6a99786e621bdb15b7a53cee40f012b8
ibran1 <ibranr@letsupgrade.in>
2023-04-19 14:59:48 +0530

    active scroll bar and highlited the section and change background

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+// for vavbar
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("header .header-container2 .navbar ul li");
+const navbar = document.querySelector("header .header-container2 .navbar");
+
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+  
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+
+      // check if the active li element is visible
+      const liLeft = li.offsetLeft;
+      const liRight = liLeft + li.offsetWidth;
+      const navbarLeft = navbar.scrollLeft;
+      const navbarRight = navbarLeft + navbar.offsetWidth;
+
+      if (liLeft < navbarLeft || liRight > navbarRight) {
+        navbar.scroll({
+          left: liLeft - navbar.offsetWidth / 2 + li.offsetWidth / 2,
+          behavior: "smooth"
+        });
+      }
+    }
+  });
+};
+
+
 let faq = document.getElementsByClassName("year-box-question");
 let i;
 for (i = 0; i < faq.length; i++) {
